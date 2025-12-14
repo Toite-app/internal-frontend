7786095bd25f3b630dcec680874e838401a04f0d
Yefrosynii Kolenko <contact@yefro.dev>
2025-12-14 14:31:09 +0200

    fix: home page redirect doesn't worked due to unsetted locale

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,11 @@
 import { redirect } from "@/navigation";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function Home() {
+type Props = {
+  params: { locale: string };
+};
+
+export default function Home({ params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
   redirect("/sign-in");
 }
